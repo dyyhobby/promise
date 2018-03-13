2e3de4c7e859050f5724cce30006f8de41b944e3
dyyhobby <dyy@dyy.name>
2018-03-13 19:36:38 +0800

    发布

--- resources/assets/js/components/Clock.js
+++ resources/assets/js/components/Clock.js
@@ -38,7 +38,7 @@ export default class Clock extends Component {
 
         return (
             <div>
-                <div className="text-center">Retr0 And AnRuo</div>
+                <div className="text-center text-danger">Retr0 @ 安若呀</div>
 
                 <div className="text-center">我们已经在一起 { this.state.date.diff(Moment("20180214"),'years') } 年
                     { Moment().diff(Moment("20180214"),'months') } 月
