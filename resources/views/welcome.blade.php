07486f8a937fcbe195f332df18538a668d83b007
dyyhobby <dyy@dyy.name>
2018-03-13 19:04:44 +0800

    上线

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Laravel</title>
+        <title>一念起、心欢喜</title>
         <!-- Fonts -->
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
