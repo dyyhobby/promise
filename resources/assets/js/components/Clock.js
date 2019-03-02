7935aa69db3be4dd79735a590d7cd314745fb1d7
Retr0 <dyy@dyy.name>
2019-03-02 20:17:29 +0800

    代码优化

--- resources/assets/js/components/Clock.js
+++ resources/assets/js/components/Clock.js
@@ -39,7 +39,6 @@ export default class Clock extends Component {
         return (
             <div>
                 <div className="text-center text-danger">Retr0 @ 安若呀</div>
-
                 <div className="text-center">我们已经在一起 { Moment().diff(Moment("20180214"),'days') } 天。</div>
                 <div className="text-center">最近的你情绪总是莫名其妙的低落。 -- 2018-11-04</div>
                 <div className="text-center">我们订婚了。 -- 2019-02-03</div>
